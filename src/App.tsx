e2401ddb6bf1097cce5b68c9a40fc04f648b81b6
richfjiq <rfjiq1986@gmail.com>
2023-03-24 15:25:24 -0600

    chore: add useRandom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
-import { useQuery } from '@tanstack/react-query';
-import { useEffect, useReducer, useState } from 'react';
 import './App.css';
-
-const getRandomNumberFromApi = async (): Promise<number> => {
-  const res = await fetch(
-    'https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new'
-  );
-  const numberString = await res.text();
-  return +numberString;
-};
+import { useRandom } from './hooks/useRandom';
 
 const App = () => {
-  const query = useQuery(['randomNumber'], getRandomNumberFromApi);
+  const query = useRandom();
 
   return (
     <div className="App App-header">
